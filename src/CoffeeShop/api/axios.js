30f57b949ee1ab6e18255fce0a866dd4d1d2ec53
thamizh9183 <kavithamil956@gmail.com>
2026-02-12 18:11:50 +0530

    Update axios.js

--- src/CoffeeShop/api/axios.js
+++ src/CoffeeShop/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://brew-heaven-backend-tk0o.onrender.com/api"
+});,
 });
 
 // 🔐 attach token
